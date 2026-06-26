9d8bc64942867f29dd336a6ebd0238758c4f4918
Kapil Yadav <yadavkapil0706@gmail.com>
2026-06-26 09:49:18 +0530

    add

--- main.py
+++ main.py
@@ -1,40 +1,24 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Request
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class User(BaseModel):
-    id: int
-    name: str
-    email: str
-    password: str
-
-
-class UserResponse(BaseModel):
-    id: int
-    name: str
-    email: str
-
-
-@app.get("/users/{user_id}", response_model=UserResponse)
-def get_user(user_id: int):
-    mock_user = User(
-        id=user_id, name="John Doe", email="kapil@gmail.com", password="securepassword"
+class ItemNotFoundException(HTTPException):
+    def __init__(self, item_id: int):
+        self.item_id = item_id
+        
+@app.exception_handler(ItemNotFoundException)
+def item_not_found_exception_handler(request: Request, exc: ItemNotFoundException):
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"message": f"Item with ID {exc.item_id} not found.", "status": "error"},
     )
-    return mock_user
-
-
-@app.post(
-    "/create_user", status_code=status.HTTP_201_CREATED, response_model=UserResponse
-)
-def create_user(user: User):
-    return {"message": "User created successfully"}
-
 
 @app.get("/items/{item_id}")
 def get_item(item_id: int):
     if item_id != 1:
-        raise HTTPException(status_code=404, detail="Item not found")
+        raise ItemNotFoundException(item_id)
 
     return {"message": "Item found", "item_id": item_id}
